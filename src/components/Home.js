d423121115142385ec30a1963bc2c44fb6ffe005
Alyssa Jackson <alyssajackson@rescomp-19-388097.stanford.edu>
2020-05-13 17:02:09 -0700

    home button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from "react";
 import '../sass/Home.sass'
+import {Link} from 'react-router-dom'
 
 function Home(){
   return (
     <div className="home">
-      <h1>Start shopping!</h1>
+      <Link to='/page'><h1>Start shopping!</h1></Link>
       <img src="https://images.unsplash.com/photo-1535972976071-2dccec4adc83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
       <img src="https://images.unsplash.com/photo-1554141220-83411835a60b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=978&q=80"/>
       <img src="https://images.unsplash.com/photo-1496217590455-aa63a8350eea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"/>
